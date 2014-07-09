8d437fdb3d00d227cbd306be3e33e5d7c69402d3
Daniel <d.f.locked@gmail.com>
2014-07-09 19:56:54 -0300

    added slim REST API client with working MySQL access

--- rest/index.php
+++ rest/index.php
@@ -1,185 +1,135 @@
-<!DOCTYPE html>
-<html>
-  <head>
-	<title>Rest API test</title>
-    <?php
+<?php
+header('Access-Control-Allow-Origin: *');
 
-abstract class API
-{
-    /**
-     * Property: method
-     * The HTTP method this request was made in, either GET, POST, PUT or DELETE
-     */
-    protected $method = '';
-    /**
-     * Property: endpoint
-     * The Model requested in the URI. eg: /files
-     */
-    protected $endpoint = '';
-    /**
-     * Property: verb
-     * An optional additional descriptor about the endpoint, used for things that can
-     * not be handled by the basic methods. eg: /files/process
-     */
-    protected $verb = '';
-    /**
-     * Property: args
-     * Any additional URI components after the endpoint and verb have been removed, in our
-     * case, an integer ID for the resource. eg: /<endpoint>/<verb>/<arg0>/<arg1>
-     * or /<endpoint>/<arg0>
-     */
-    protected $args = Array();
-    /**
-     * Property: file
-     * Stores the input of the PUT request
-     */
-     protected $file = Null;
+/*
+require 'vendor/autoload.php';
+f you have any questions for Rob (the greenhouse project coordinator) please email them to me by Friday at noon as that is when I’ll be firing off the first batch of questions to him.
 
-    /**
-     * Constructor: __construct
-     * Allow for CORS, assemble and pre-process the data
-     */
-    public function __construct($request) {
-        header("Access-Control-Allow-Orgin: *");
-        header("Access-Control-Allow-Methods: *");
-        header("Content-Type: application/json");
+Also, we have acquired some server space (thanks to Jonathon Amyotte), here are the details to access it:
 
-        $this->args = explode('/', rtrim($request, '/'));
-        $this->endpoint = array_shift($this->args);
-        if (array_key_exists(0, $this->args) && !is_numeric($this->args[0])) {
-            $this->verb = array_shift($this->args);
+Website URL: https://projects.cs.dal.ca/greenhouse (Redirection to HTTPS from HTTP is automatic and always in place).
+
+SSH Hostname: projects.cs.dal.ca
+SSH Username: greenhouse
+SSH Password: UqWA%wiZ7Z4vcpLV
+
+MySQL Username: greenhouse
+MySQL Password: oG9gk=yk7DMDUxFJ
+MySQL Hostname: localhost
+MySQL Database: greenhouse
+
+- The website file space is accessible via the "website" symlink when you login via SSH/SFTP.
+- You can change the password at the command line with the "passwd" command.
+- There is no restriction on where you can connect from over SSH/SFTP (no regular FTP) or HTTP/HTTPS. Most other ports are blocked.
+
+I ask that no one change any of these passwords as everyone will need to be able to access them!
+
+Also, please do not share them with anyone outside of this class.
+
+If anyone has any problems or needs any clarification, please do not hesitate to email me.
+
+Thanks,
+Mo Tawakol
+---------------------------------
+
+<?php
+    $col = "CLASS";
+    $q = "SELECT DISTINCT $col FROM $tableName ORDER BY $col";
+    $r = mysql_query($q,$dbc);
+    if($r)
+    {
+        echo "<select id=\"$col\" class=\"form-control\" onchange=\"OnChange(this.form.CLASS)\" name=\"$col\">\n";
+        echo "<option selected value=\"\"> </option>\n";
+                        while ($row = mysql_fetch_array($r))
+                        {
+                            echo "<option value=\"{$row[$col]}\">{$row[$col]}</option>\n";
+                        }
+                                    echo "</select>\n";
+    }
+?>
+
+INSERT INTO `ubicomp`.`test` (`name`) VALUES ('example');
+*/
+
+    require 'Slim/Slim.php';
+    \Slim\Slim::registerAutoloader();
+
+    $app = new \Slim\Slim();
+
+
+    DEFINE ('DB_USER', 'greenhouse');
+    DEFINE ('DB_PASSWORD', 'oG9gk=yk7DMDUxFJ');
+    DEFINE ('DB_HOST', 'localhost');
+    DEFINE ('DB_NAME', 'test');
+    DEFINE ('TABLENAME','data');
+    // $tableName = "data";
+
+    // $dbc = @mysql_connect (DB_HOST, DB_USER,DB_PASSWORD)
+    // OR die ('Could not connect to MySQL: ' . mysql_connect_error());
+
+    // $db = @mysql_select_db(DB_NAME,$dbc);
+    // mysql_set_charset('utf8',$dbc);
+    // $GLOBALS['dbc'] = $dbc;
+
+    $app->get('/',function () {
+        //Connects to the mySQL database
+        $dbc = @mysql_connect (DB_HOST, DB_USER,DB_PASSWORD)
+        OR die ('Could not connect to MySQL: ' . mysql_connect_error());
+        $db = @mysql_select_db(DB_NAME,$dbc);
+        mysql_set_charset('utf8',$dbc);
+        $col = "name";
+        $q = "SELECT * from ".TABLENAME;
+        $r = mysql_query($q,$dbc);
+
+        while ($row = mysql_fetch_array($r))
+        {
+            $arr[] = $row;
+            printf("ID: %s  Name: %s", $row[0], $row[1]);
         }
+        return json_encode($arr);
+    });
+    /*
+    $app->post('/post', 'post');
+    $app->put('/put', 'update');
+    $app->delete('/delete', 'delete');
+    */
 
-        $this->method = $_SERVER['REQUEST_METHOD'];
-        if ($this->method == 'POST' && array_key_exists('HTTP_X_HTTP_METHOD', $_SERVER)) {
-            if ($_SERVER['HTTP_X_HTTP_METHOD'] == 'DELETE') {
-                $this->method = 'DELETE';
-            } else if ($_SERVER['HTTP_X_HTTP_METHOD'] == 'PUT') {
-                $this->method = 'PUT';
-            } else {
-                throw new Exception("Unexpected Header");
-            }
-        }
-
-        switch($this->method) {
-        case 'DELETE':
-        case 'POST':
-            $this->request = $this->_cleanInputs($_POST);
-            break;
-        case 'GET':
-            $this->request = $this->_cleanInputs($_GET);
-            break;
-        case 'PUT':
-            $this->request = $this->_cleanInputs($_GET);
-            $this->file = file_get_contents("php://input");
-            break;
-        default:
-            $this->_response('Invalid Method', 405);
-            break;
-        }
+    function post() {
+        echo "POST";
+    }
+    function put() {
+        echo "pit"; 
+    }
+    function delete() {
+        echo "delete";
     }
 
+    $app->run();
+/*
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
 
-    public function processAPI() {
-        if ((int)method_exists($this, $this->endpoint) > 0) {
-            return $this->_response($this->{$this->endpoint}($this->args));
-        }
-        return $this->_response("No Endpoint: $this->endpoint", 404);
-    }
+$app = new \Slim\Slim();
 
-    private function _response($data, $status = 200) {
-        header("HTTP/1.1 " . $status . " " . $this->_requestStatus($status));
-        return json_encode($data);
-    }
+$app->get('/', 'get');
+$app->get('/post', 'post');
+$app->get('/put', 'update');
+$app->get('/delete', 'delete');
 
-    private function _cleanInputs($data) {
-        $clean_input = Array();
-        if (is_array($data)) {
-            foreach ($data as $k => $v) {
-                $clean_input[$k] = $this->_cleanInputs($v);
-            }
-        } else {
-            $clean_input = trim(strip_tags($data));
-        }
-        return $clean_input;
-    }
+function get() {
 
-    private function _requestStatus($code) {
-        $status = array(  
-            200 => 'OK',
-            404 => 'Not Found',   
-            405 => 'Method Not Allowed',
-            500 => 'Internal Server Error',
-        ); 
-        return ($status[$code])?$status[$code]:$status[500]; 
-    }
 }
 
+function post() {
+    
+}
+function put() {
+    
+}
+function delete() {
+    
+}
 
-class MyAPI extends API
-{
-    protected $User;
-
-    public function __construct($request, $origin) {
-        parent::__construct($request);
-
-        // Abstracted out for example
-        $APIKey = new UBIcomp\APIKey();
-        $User = new UBIcomp\User();
-
-        if (!array_key_exists('apiKey', $this->request)) {
-            throw new Exception('No API Key provided');
-        } else if (!$APIKey->verifyKey($this->request['apiKey'], $origin)) {
-            throw new Exception('Invalid API Key');
-        } else if (array_key_exists('token', $this->request) &&
-             !$User->get('token', $this->request['token'])) {
-
-            throw new Exception('Invalid User Token');
-        }
-
-        $this->User = $User;
-    }
-
-    /**
-     * Example of an Endpoint
-     */
-     protected function example() {
-        if ($this->method == 'GET') {
-            return "Your name is " . $this->User->name;
-        } else {
-            return "Only accepts GET requests";
-        }
-     }
- }
-    ?>
-  </head>
-
-  <body>
-	<h1>This is a test</h1>
-
-  <form action="<?php $_PHP_SELF ?>" method="post">
- 
-  Your Name: <input type="text" name="request" />
- 
-  <input type="submit" />
- 
-  </form>
-
-    <?php
-    // Requests from the same server don't have a HTTP_ORIGIN header
-    if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
-        $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
-    }
-
-
-
-
-    try {
-        $API = new MyAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
-        echo $API->processAPI();
-    } catch (Exception $e) {
-        echo json_encode(Array('error' => $e->getMessage()));
-    }
-    ?>
-  </body>
-</html>
+$app->run();
+*/
+?>
